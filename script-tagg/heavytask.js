9d3bf7a896e6e60c09a15231ffae2bc7783cfdf3
flindr <lindroth.fredrik@gmail.com>
2021-03-10 05:07:24 +0100

    first commit

---
+++ script-tagg/heavytask.js
@@ -0,0 +1,16 @@
+function heavyTask() {
+    const heavyTaskTime = new Date();
+    console.log("Starting heavy task", heavyTaskTime.toJSON());
+    let sum = 0;
+
+    for (let i = 0; i < 1000000000; i++) {
+        sum += i;
+    }
+
+    console.log(
+        "Finished heavy task. Elapsed: ",
+        new Date().getTime() - heavyTaskTime.getTime()
+    );
+}
+
+heavyTask();
